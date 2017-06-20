b637c6c1d2aba40e77fdde580a59445280d78ce4
VasanthakumariS <vasantha.vlr99@gmail.com>
2017-06-20 15:18:41 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  
+  int i;
+  char str[50];
+  clrscr();
+  for(i=1;i<=5;i++)
+  {
+     printf("\n%s",'Hello');
+  }
+  getch();
+}
+  
+  
