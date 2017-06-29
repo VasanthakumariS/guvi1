2b9ffd3c5500c0bc456445d43e3a08a11d466a5c
VasanthakumariS <vasantha.vlr99@gmail.com>
2017-06-29 12:57:46 +0530

    Create GREAT.C

---
+++ GREAT.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+  int a=5,b=8,c=9;
+  if(a>b&&a>c)
+  {
+      printf("\n a is greatest number");
+      else
+      if(b>c)
+      {
+        printf("\n b is greatest number");
+      }
+      else
+      {
+        printf("\n c is greatest number");
+      }
+   }
+}
+  
